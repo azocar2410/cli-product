7523aabc3ccd33e62a5d64adede5f02f1d8bec27
azocar2410 <arturo-2410@hotmail.es>
2026-07-02 20:39:18 -0300

    Fix app.listen syntax by adding missing parentheses

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`
+                                  
+));
